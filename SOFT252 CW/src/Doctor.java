f04095dc3351fc11cd3f9e2b10ac448fe7dae0cf
tomo888 <33130687+tomo888@users.noreply.github.com>
2019-01-16 22:36:55 +0000

    Version 1

---
+++ SOFT252 CW/src/Doctor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tjotoole
+ */
+public class Doctor {
+    private String forename;
+    private String surname;
+    private int age;
+    private 
+    private String specialisation;
+    private double wage;
+    
+}
